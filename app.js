10a4a0c61f16f0c12b2b5eb1e5012414d3ea60d1
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2023-08-31 13:13:33 +0530

    Laying the foundation

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React"
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react";
+import  ReactDOM  from "react-dom/client"; 
 
-// createElement method takes three arguments 
-// 1.Name, 2. object of attributes, 3. children(have to pass in array if more than one children is there) 
-const parent = React.createElement("parent", {}, [
-  React.createElement(
-    "child",
-    {},
-    React.createElement("h1", {}, "I am not the ultimate ")
-  ),
-  React.createElement(
-    "child",
-    {},
-    React.createElement("h1", {}, "I am the ultimate ")
-  ),
-]);
-// render method injects the argument to the parent this is root, it injects the argument to the root. 
+const jsxHeading = <h1>Hai</h1>
 
-root.render(parent);
-console.log(parent);
+const title = () => ( <h2> Hello React Namaste</h2>)
+const Title = () => {
+  return <h1> Hello </h1>
+}
+
+const Header= () => {
+  return (
+  <div>
+    <h1>  Namaste React</h1>
+    {Title}
+    <Title />
+    <Title></Title>
+    {title()}
+  </div>
+  );
+};
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
+
+
+
